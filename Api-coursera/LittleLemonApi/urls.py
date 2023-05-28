aef203115a2a74de3a537592bca4e02c85f52152
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-28 03:56:09 +0100

    api_conf

--- Api-coursera/LittleLemonApi/urls.py
+++ Api-coursera/LittleLemonApi/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
+#from django.urls import path
+#from . import views
 
-urlpatterns = [
-    path('ratings', views.RatingsView.as_view()),
-    path('books', views.books),
-]
+#urlpatterns = [
+    #path('ratings', views.RatingsView.as_view()),
+ #   path('/', views.books),
+#]
