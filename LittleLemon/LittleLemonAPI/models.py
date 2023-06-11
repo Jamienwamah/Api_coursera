d7cbdca12660c42e8f8a5a999207488e2d47f18a
oishbdh <ikechukwuarinze614@gmail.com>
2023-06-11 16:07:58 +0100

    api

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length = 255)
+
+
+
 class MenuItems(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=3, decimal_places=2)
     inventory = models.SmallIntegerField()
+    #Creating a relational model
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default = 1)
     
     def __self__(self):
         return self.title
--- LittleLemon/LittleLemonAPI/serializers.py
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 from .models import MenuItems
+from decimal import Decimal
 #Create serializers here
 
 class MenuItemSerializer(serializers.ModelSerializer):
+#Lets change the name of a field using seializers
+    stock = serializers.IntegerField(source = 'inventory')
+   #linking the  calculate tax method as a new field in serializer
+    price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     class Meta:
         model = MenuItems
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['title', 'price', 'stock', 'price_after_tax']
+        
+    #Introducing a new field in serializer
+    def calculate_tax(self, product:MenuItems):
+        return product.price * Decimal(1.1)
     
