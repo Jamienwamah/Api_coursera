106cc43d648cda39ed40eb5f574777ba76b6c09e
oishbdh <ikechukwuarinze614@gmail.com>
2023-06-15 22:02:46 +0100

    api

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        rest_framework.renderers.JSONRenderer,
+        rest_framework.renderers.BrowsableAPIRenderer,
+        rest_framework_xml.renderers.XMLRenderer,
+    ]
+}
